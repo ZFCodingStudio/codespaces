916c18f74b6e83f8d14adde9c6787be083bf903e
guima <guimasharing@gmail.com>
2023-12-15 09:07:30 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("来自codespaces的问候");
+});
+
+app.post("/", (req, res) => {
+  res.send("来自codespaces的问候,post那边的朋友");
+});
+
+app.listen(port, () => {
+  console.log(`express js 正常运行于端口 ${port}`);
+});
